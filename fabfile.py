a08b80fe13665ee692309f08987cd53f1f49611e
kokeshii <darthmincho@gmail.com>
2015-02-12 20:42:12 -0600

    Add template tags to fabfile.

--- fabfile.py
+++ fabfile.py
@@ -1,11 +1,22 @@
+from __future__ import with_statement
 from fabric.api import *
+from fabric.context_managers import shell_env
+from deploy import create_droplet
+
 
 """
 Base configuration
 """
-env.project_name = '{{ project_name }}'
-env.database_password = ''
-env.path = '/home/django/{{ project_name }}' % env
 
-def setup():
-	print 'hello'
+def droplet():
+	create_droplet()
+
+def deploy():
+	code_dir = '/home/django/{{ project_name }}'
+	with cd(code_dir):
+		with(shell_env(DJANGO_SETTINGS_MODULE='{{ project_name }}.settings.production')):
+			run('git pull')
+			run('pip install -r requirements.txt')
+			run('./manage.py migrate')
+			run('./manage.py collectstatic --noinput')
+			run('service gunicorn restart')
